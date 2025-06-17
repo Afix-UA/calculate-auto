3c633ea687663bdf56aab5fa873ee659d2a801e6
Afix-UA <93038253+Afix-UA@users.noreply.github.com>
2025-06-18 01:45:19 +0300

    Add files via upload

--- js/dev/index.js
+++ js/dev/index.js
@@ -35,6 +35,81 @@
     fetch(link.href, fetchOpts);
   }
 })();
+function getHash() {
+  if (location.hash) {
+    return location.hash.replace("#", "");
+  }
+}
+function setHash(hash) {
+  hash = hash ? `#${hash}` : window.location.href.split("#")[0];
+  history.pushState("", "", hash);
+}
+let slideUp = (target, duration = 500, showmore = 0) => {
+  if (!target.classList.contains("--slide")) {
+    target.classList.add("--slide");
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.height = `${target.offsetHeight}px`;
+    target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = showmore ? `${showmore}px` : `0px`;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+      target.hidden = !showmore ? true : false;
+      !showmore ? target.style.removeProperty("height") : null;
+      target.style.removeProperty("padding-top");
+      target.style.removeProperty("padding-bottom");
+      target.style.removeProperty("margin-top");
+      target.style.removeProperty("margin-bottom");
+      !showmore ? target.style.removeProperty("overflow") : null;
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+      target.classList.remove("--slide");
+      document.dispatchEvent(new CustomEvent("slideUpDone", {
+        detail: {
+          target
+        }
+      }));
+    }, duration);
+  }
+};
+let slideDown = (target, duration = 500, showmore = 0) => {
+  if (!target.classList.contains("--slide")) {
+    target.classList.add("--slide");
+    target.hidden = target.hidden ? false : null;
+    showmore ? target.style.removeProperty("height") : null;
+    let height = target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = showmore ? `${showmore}px` : `0px`;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.height = height + "px";
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    window.setTimeout(() => {
+      target.style.removeProperty("height");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+      target.classList.remove("--slide");
+      document.dispatchEvent(new CustomEvent("slideDownDone", {
+        detail: {
+          target
+        }
+      }));
+    }, duration);
+  }
+};
 let bodyLockStatus = true;
 let bodyUnlock = (delay = 500) => {
   if (bodyLockStatus) {
@@ -67,6 +142,137 @@ let bodyLock = (delay = 500) => {
     }, delay);
   }
 };
+function dataMediaQueries(array, dataSetValue) {
+  const media = Array.from(array).filter((item) => item.dataset[dataSetValue]).map((item) => {
+    const [value, type = "max"] = item.dataset[dataSetValue].split(",");
+    return { value, type, item };
+  });
+  if (media.length === 0) return [];
+  const breakpointsArray = media.map(({ value, type }) => `(${type}-width: ${value}px),${value},${type}`);
+  const uniqueQueries = [...new Set(breakpointsArray)];
+  return uniqueQueries.map((query) => {
+    const [mediaQuery, mediaBreakpoint, mediaType] = query.split(",");
+    const matchMedia = window.matchMedia(mediaQuery);
+    const itemsArray = media.filter((item) => item.value === mediaBreakpoint && item.type === mediaType);
+    return { itemsArray, matchMedia };
+  });
+}
+function tabs() {
+  const tabs2 = document.querySelectorAll("[data-fls-tabs]");
+  let tabsActiveHash = [];
+  if (tabs2.length > 0) {
+    const hash = getHash();
+    if (hash && hash.startsWith("tab-")) {
+      tabsActiveHash = hash.replace("tab-", "").split("-");
+    }
+    tabs2.forEach((tabsBlock, index) => {
+      tabsBlock.classList.add("--tab-init");
+      tabsBlock.setAttribute("data-fls-tabs-index", index);
+      tabsBlock.addEventListener("click", setTabsAction);
+      initTabs(tabsBlock);
+    });
+    let mdQueriesArray = dataMediaQueries(tabs2, "flsTabs");
+    if (mdQueriesArray && mdQueriesArray.length) {
+      mdQueriesArray.forEach((mdQueriesItem) => {
+        mdQueriesItem.matchMedia.addEventListener("change", function() {
+          setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+        });
+        setTitlePosition(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+      });
+    }
+  }
+  function setTitlePosition(tabsMediaArray, matchMedia) {
+    tabsMediaArray.forEach((tabsMediaItem) => {
+      tabsMediaItem = tabsMediaItem.item;
+      let tabsTitles = tabsMediaItem.querySelector("[data-fls-tabs-titles]");
+      let tabsTitleItems = tabsMediaItem.querySelectorAll("[data-fls-tabs-title]");
+      let tabsContent = tabsMediaItem.querySelector("[data-fls-tabs-body]");
+      let tabsContentItems = tabsMediaItem.querySelectorAll("[data-fls-tabs-item]");
+      tabsTitleItems = Array.from(tabsTitleItems).filter((item) => item.closest("[data-fls-tabs]") === tabsMediaItem);
+      tabsContentItems = Array.from(tabsContentItems).filter((item) => item.closest("[data-fls-tabs]") === tabsMediaItem);
+      tabsContentItems.forEach((tabsContentItem, index) => {
+        if (matchMedia.matches) {
+          tabsContent.append(tabsTitleItems[index]);
+          tabsContent.append(tabsContentItem);
+          tabsMediaItem.classList.add("--tab-spoller");
+        } else {
+          tabsTitles.append(tabsTitleItems[index]);
+          tabsMediaItem.classList.remove("--tab-spoller");
+        }
+      });
+    });
+  }
+  function initTabs(tabsBlock) {
+    let tabsTitles = tabsBlock.querySelectorAll("[data-fls-tabs-titles]>*");
+    let tabsContent = tabsBlock.querySelectorAll("[data-fls-tabs-body]>*");
+    const tabsBlockIndex = tabsBlock.dataset.flsTabsIndex;
+    const tabsActiveHashBlock = tabsActiveHash[0] == tabsBlockIndex;
+    if (tabsActiveHashBlock) {
+      const tabsActiveTitle = tabsBlock.querySelector("[data-fls-tabs-titles]>.--tab-active");
+      tabsActiveTitle ? tabsActiveTitle.classList.remove("--tab-active") : null;
+    }
+    if (tabsContent.length) {
+      tabsContent.forEach((tabsContentItem, index) => {
+        tabsTitles[index].setAttribute("data-fls-tabs-title", "");
+        tabsContentItem.setAttribute("data-fls-tabs-item", "");
+        if (tabsActiveHashBlock && index == tabsActiveHash[1]) {
+          tabsTitles[index].classList.add("--tab-active");
+        }
+        tabsContentItem.hidden = !tabsTitles[index].classList.contains("--tab-active");
+      });
+    }
+  }
+  function setTabsStatus(tabsBlock) {
+    let tabsTitles = tabsBlock.querySelectorAll("[data-fls-tabs-title]");
+    let tabsContent = tabsBlock.querySelectorAll("[data-fls-tabs-item]");
+    const tabsBlockIndex = tabsBlock.dataset.flsTabsIndex;
+    function isTabsAnamate(tabsBlock2) {
+      if (tabsBlock2.hasAttribute("data-fls-tabs-animate")) {
+        return tabsBlock2.dataset.flsTabsAnimate > 0 ? Number(tabsBlock2.dataset.flsTabsAnimate) : 500;
+      }
+    }
+    const tabsBlockAnimate = isTabsAnamate(tabsBlock);
+    if (tabsContent.length > 0) {
+      const isHash = tabsBlock.hasAttribute("data-fls-tabs-hash");
+      tabsContent = Array.from(tabsContent).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock);
+      tabsTitles = Array.from(tabsTitles).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock);
+      tabsContent.forEach((tabsContentItem, index) => {
+        if (tabsTitles[index].classList.contains("--tab-active")) {
+          if (tabsBlockAnimate) {
+            slideDown(tabsContentItem, tabsBlockAnimate);
+          } else {
+            tabsContentItem.hidden = false;
+          }
+          if (isHash && !tabsContentItem.closest(".popup")) {
+            setHash(`tab-${tabsBlockIndex}-${index}`);
+          }
+        } else {
+          if (tabsBlockAnimate) {
+            slideUp(tabsContentItem, tabsBlockAnimate);
+          } else {
+            tabsContentItem.hidden = true;
+          }
+        }
+      });
+    }
+  }
+  function setTabsAction(e) {
+    const el = e.target;
+    if (el.closest("[data-fls-tabs-title]")) {
+      const tabTitle = el.closest("[data-fls-tabs-title]");
+      const tabsBlock = tabTitle.closest("[data-fls-tabs]");
+      if (!tabTitle.classList.contains("--tab-active") && !tabsBlock.querySelector(".--slide")) {
+        let tabActiveTitle = tabsBlock.querySelectorAll("[data-fls-tabs-title].--tab-active");
+        tabActiveTitle.length ? tabActiveTitle = Array.from(tabActiveTitle).filter((item) => item.closest("[data-fls-tabs]") === tabsBlock) : null;
+        tabActiveTitle.length ? tabActiveTitle[0].classList.remove("--tab-active") : null;
+        tabTitle.classList.add("--tab-active");
+        setTabsStatus(tabsBlock);
+      }
+      e.preventDefault();
+    }
+  }
+}
+window.addEventListener("load", tabs);
 class Popup {
   constructor(options) {
     let config = {
@@ -371,54 +577,148 @@ class Popup {
   }
 }
 document.querySelector("[data-fls-popup]") ? window.addEventListener("load", window.flsPopup = new Popup({})) : null;
-const form = document.querySelector(".form-calc");
-if (form) {
-  let extractNumbersFromForm = function(e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const obj = {};
-    for (const [key, value] of formData.entries()) {
-      obj[key] = value;
-    }
-    const jsonStr = JSON.stringify(obj);
-    getInnerData(calculateData(jsonStr));
-  }, calculateData = function(data) {
-    const obj = JSON.parse(data);
-    let startMileage = +obj.start_mileage;
-    let fuelQuantity = +obj.fuel_quantity;
-    let totalKm = +obj.total_km;
-    let loadedKm = +obj.loaded_km;
-    let cargo = +obj.cargo;
-    let fuelConsumptionEmpty = Math.round(totalKm) * 30 / 100;
-    let cargoFuelConsumption = Math.round(loadedKm * cargo * 1.3 / 100);
-    let totalFuelConsumption = fuelConsumptionEmpty + cargoFuelConsumption;
-    let finalMileage = startMileage + totalKm;
-    let remainingFuel = fuelQuantity - totalFuelConsumption;
-    return {
-      startMileage,
-      fuelQuantity,
-      totalKm,
-      loadedKm,
-      cargo,
-      fuelConsumptionEmpty,
-      cargoFuelConsumption,
-      finalMileage,
-      remainingFuel,
-      totalFuelConsumption
-    };
-  }, getInnerData = function(_calculateData) {
-    const outputDiv = document.querySelector("[data-output]");
-    if (!outputDiv) return;
-    outputDiv.innerHTML = `
-        <h2 class="popup__title">Ось твої підрахунки</h2>
-    <p>Загальний пробіг: <strong>${_calculateData.finalMileage} км</strong></p>
-    <p>витрата пального: <strong>${_calculateData.totalFuelConsumption} л</strong></p>
-    <p>Залишок пального: <strong>${_calculateData.remainingFuel} л</strong></p>
-    <p>Витрата пального без вантажу: <strong>${_calculateData.fuelConsumptionEmpty} л</strong></p>
-    <p>Витрата пального з вантажем: <strong>${_calculateData.cargoFuelConsumption} л</strong></p>
-        `;
-    window.flsPopup.open("popup");
+const formTonKm = document.querySelector(".tonKm");
+const formMotoHours = document.querySelector(".motoHours");
+function addInputStorage() {
+  const _dataCalcTonKm = JSON.parse(localStorage.getItem("dataTonKm"));
+  if (_dataCalcTonKm) {
+    formTonKm.start_mileage.value = _dataCalcTonKm.Mileage;
+    formTonKm.fuel_per100.value = _dataCalcTonKm.fuelPer;
+  }
+  const _dataCalcMotoHours = JSON.parse(
+    localStorage.getItem("dataCalcMotoHours")
+  );
+  if (_dataCalcMotoHours) {
+    formMotoHours.start_mileage.value = _dataCalcMotoHours.Mileage;
+    formMotoHours.fuel_per100.value = _dataCalcMotoHours.fuelPer;
+    formMotoHours.fuelPerMH.value = _dataCalcMotoHours.PerMH;
+  }
+}
+addInputStorage();
+if (formTonKm) {
+  formTonKm.addEventListener("submit", extractNumbersFromFormTonKm);
+}
+function extractNumbersFromFormTonKm(e) {
+  e.preventDefault();
+  const formData = new FormData(formTonKm);
+  const objTonKm = {};
+  for (const [key, value] of formData.entries()) {
+    objTonKm[key] = value;
+  }
+  const jsonStrTonKm = JSON.stringify(objTonKm);
+  const calcResult = calculateDataTonKm(jsonStrTonKm);
+  getInnerData(0, calcResult);
+}
+function calculateDataTonKm(data) {
+  const obj = JSON.parse(data);
+  localStorage.setItem("dataCalc", data);
+  const startMileage = +obj.start_mileage;
+  const fuelQuantity = +obj.fuel_quantity;
+  const totalKm = +obj.total_km;
+  const fuelPer100 = +obj.fuel_per100;
+  const loadedKm = +obj.loaded_km;
+  const cargo = +obj.cargo;
+  const fuelConsumptionEmpty = Math.round(totalKm) * fuelPer100 / 100;
+  const cargoFuelConsumption = Math.round(loadedKm * cargo * 1.3 / 100);
+  const totalFuelConsumption = fuelConsumptionEmpty + cargoFuelConsumption;
+  const finalMileage = startMileage + totalKm;
+  const remainingFuel = fuelQuantity - totalFuelConsumption;
+  setLocal(0, finalMileage, fuelPer100);
+  return {
+    startMileage,
+    fuelQuantity,
+    totalKm,
+    loadedKm,
+    cargo,
+    fuelConsumptionEmpty,
+    cargoFuelConsumption,
+    finalMileage,
+    remainingFuel,
+    totalFuelConsumption
   };
-  var extractNumbersFromForm2 = extractNumbersFromForm, calculateData2 = calculateData, getInnerData2 = getInnerData;
-  form.addEventListener("submit", extractNumbersFromForm);
+}
+if (formMotoHours) {
+  formMotoHours.addEventListener("submit", extractNumbersFromFormformMotoHours);
+}
+function extractNumbersFromFormformMotoHours(e) {
+  e.preventDefault();
+  const formDataMotoHours = new FormData(formMotoHours);
+  const objMotoHours = {};
+  for (const [key, value] of formDataMotoHours.entries()) {
+    objMotoHours[key] = value;
+  }
+  const jsonStrMotoHours = JSON.stringify(objMotoHours);
+  const calcResult = calculateDataMotoHours(jsonStrMotoHours);
+  getInnerData(1, calcResult);
+}
+function calculateDataMotoHours(data) {
+  const obj = JSON.parse(data);
+  localStorage.setItem("dataCalc", data);
+  const startMileage = +obj.start_mileage;
+  const fuelQuantity = +obj.fuel_quantity;
+  const totalKm = +obj.total_km;
+  const fuelPer100 = +obj.fuel_per100;
+  const engineHoursWorked = +obj.engineHoursWorked;
+  const fuelPerMH = +obj.fuelPerMH;
+  console.log(fuelPer100);
+  const PerMH = engineHoursWorked * fuelPerMH;
+  const totalFuelConsumption = Math.round(totalKm * fuelPer100 / 100 + PerMH);
+  const finalMileage = Math.round(startMileage + totalKm);
+  const remainingFuel = Math.round(fuelQuantity - totalFuelConsumption);
+  setLocal(1, finalMileage, fuelPerMH, fuelPer100);
+  return {
+    startMileage,
+    fuelQuantity,
+    totalKm,
+    engineHoursWorked,
+    fuelPerMH,
+    PerMH,
+    totalFuelConsumption,
+    finalMileage,
+    remainingFuel
+  };
+}
+function getInnerData(checkbox, _calculateData) {
+  console.log(_calculateData);
+  const outputDiv = document.querySelector("[data-output]");
+  if (!outputDiv) return;
+  if (+checkbox === 0) {
+    outputDiv.innerHTML = `
+      <h2 class="popup__title">Ось твої підрахунки</h2>
+      <p>Загальний пробіг: <strong>${_calculateData.finalMileage} км</strong></p>
+      <p>Витрата пального: <strong>${_calculateData.totalFuelConsumption} л</strong></p>
+      <p>Залишок пального: <strong>${_calculateData.remainingFuel} л</strong></p>
+      <p>Витрата пального без вантажу: <strong>${_calculateData.fuelConsumptionEmpty} л</strong></p>
+      <p>Витрата пального з вантажем: <strong>${_calculateData.cargoFuelConsumption} л</strong></p>
+    `;
+  } else if (+checkbox === 1) {
+    outputDiv.innerHTML = `
+      <h2 class="popup__title">Ось твої підрахунки</h2>
+      <p>Загальний пробіг: <strong>${_calculateData.finalMileage} км</strong></p>
+      <p>Витрата пального: <strong>${_calculateData.totalFuelConsumption} л</strong></p>
+      <p>Залишок пального: <strong>${_calculateData.remainingFuel} л</strong></p>
+    `;
+  }
+  if (window.flsPopup) {
+    window.flsPopup.open("popup");
+  }
+}
+function setLocal(checkbox, finalMileage, fuelValue, fuelPer100) {
+  if (+checkbox === 0) {
+    const dataCalcTonKm = {
+      Mileage: finalMileage,
+      fuelPer: fuelValue
+    };
+    localStorage.setItem("dataTonKm", JSON.stringify(dataCalcTonKm));
+  } else if (+checkbox === 1) {
+    const dataCalcMotoHours = {
+      Mileage: finalMileage,
+      PerMH: fuelValue,
+      fuelPer: fuelPer100
+    };
+    localStorage.setItem(
+      "dataCalcMotoHours",
+      JSON.stringify(dataCalcMotoHours)
+    );
+  }
 }
